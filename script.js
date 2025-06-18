aef927f657e662255a6faf9971e47694ed97c8e6
Guilherme <guilherme.d.santos17@aluno.senai.br>
2025-06-18 12:18:26 -0300

    Incluindo Nota3

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function calcularMedia() {
     //Paga os valores dos campos
     var n1 = parseFloat(document.getElementById('nota1').value);
     var n2 = parseFloat(document.getElementById('nota2').value);
+    var n3 = parseFloat(document.getElementById('nota3').value);
 
-    if (isNaN(n1) || isNaN(n2)) {
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
         alert("Por favor, insira notas válidas");
         return;
     }
 
     //calcular a Média
-    var media = (n1+n2) /2;
+    var media = (n1+n2+n3) /3;
 
     //mostra o resutado
     document.getElementById('resultado').innerText = "A Média é: " + media.toFixed(2);
